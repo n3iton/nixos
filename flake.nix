8454def6d21a1ab2cc8e8eb3e50feb0f8b57c086
neiton <neiton@gmail.com>
2025-06-19 23:27:15 +0300

    fix: add specialArgs to x280 NixOS output

--- flake.nix
+++ flake.nix
@@ -48,6 +48,7 @@
 
         x280 = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
           modules = [
             ./hosts/x280/configuration.nix
           ];
