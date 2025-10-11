a208217ab2a88c25011cfcb6b3b431ab26a4a17c
neiton <neiton@gmail.com>
2025-10-12 02:24:34 +0300

    refactor(users): add formatting

--- modules/users.nix
+++ modules/users.nix
@@ -4,7 +4,7 @@
   sops.secrets.root-password.neededForUsers = true;
 
   users.users.root.hashedPasswordFile = config.sops.secrets.root-password.path;
-  
+
   users.users.neiton = {
     isNormalUser = true;
     description = "neiton";
