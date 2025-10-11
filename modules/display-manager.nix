6d692ecdbb17b10565e807d556b006df1f176d22
neiton <neiton@gmail.com>
2025-10-12 02:11:58 +0300

    feat: change sddm theme

--- modules/display-manager.nix
+++ modules/display-manager.nix
@@ -1,11 +1,21 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  environment.systemPackages = [
+    (pkgs.sddm-astronaut.override {
+      embeddedTheme = "purple_leaves";
+    })
+  ];
+
   # Включаем SDDM как дисплей-менеджер
   services.displayManager.sddm = {
     enable = true;
+    package = pkgs.kdePackages.sddm;
+    extraPackages = [
+      pkgs.kdePackages.qtmultimedia
+    ];
     wayland.enable = true; # 👈 это обязательная строка, если не используешь X11
-    theme = "maldives";
+    theme = "sddm-astronaut-theme";
   };
 
   # Указываем, что будем использовать Hyprland
