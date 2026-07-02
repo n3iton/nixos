94725cf67b7e16f571920e57593c1f9715ee5726
neiton <neitonmatisso@gmail.com>
2026-07-03 00:37:48 +0300

    feat: add almost all packages to homebrew

--- hosts/macbook/darwin-configuration.nix
+++ hosts/macbook/darwin-configuration.nix
@@ -9,16 +9,54 @@
 
   system.primaryUser = "nikita";
 
+  programs.fish.enable = true;
+
+  environment.shells = [ pkgs.fish ];
+
   homebrew = {
     enable = true;
-    
+
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+    };
+
     casks = [
       "loop-messenger"
       "docker-desktop"
       "firefox"
       "kontur-talk"
+      "soduto"
+      "happ"
+      "visual-studio-code"
+      "zoom"
+      "monitorcontrol"
+      "cursor"
+      "keepassxc"
+      "obsidian"
+    ];
+
+    brews = [
+      "kubebuilder"
+      "go-task"
+      "werf"
+      "yq"
+      "trivy"
+      "pre-commit"
+      "syncthing"
+      "starship"
+      "nixfmt"
+      "go"
+      "tmux"
+      "neovim"
+      "tealdeer"
+      "tree"
+      "dive"
+      "stow"
+      "go-task"
+      "crane"
     ];
   };
-  
+
   system.stateVersion = 5;
 }
--- hosts/macbook/home.nix
+++ hosts/macbook/home.nix
@@ -7,29 +7,9 @@
   home.stateVersion = "25.11";
 
   home.packages = with pkgs; [
-    keepassxc
-    syncthing
-    starship
-    vscode
-    postman
-
-    nil
-    nixfmt
-
-    unstable.go
-
-    ghostty-bin
-
-    tmux
-    fish
-    neovim
-
-    tealdeer
-    tree
-
-    obsidian
-
-    librewolf
+    nixd
+    unstable.clickhouse
+    ayugram-desktop
   ];
 
 }
